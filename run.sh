c9db7f71e7d4f7d17c8cd82a5082e349ddc54c89
bmatt <bmatt@nuc01.attlocal.net>
2019-09-28 02:02:56 +0000

    Updated to 5.11.46

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [[ ! -f /usr/lib/unifi/data/keystore ]]; then
+if [[ ! -f /usr/lib/unifi/data/keystore ]]
+then
     keytool -genkey -keyalg RSA -alias unifi -keystore /usr/lib/unifi/data/keystore -storepass aircontrolenterprise -keypass aircontrolenterprise -validity 1825 -keysize 4096 -dname "cn=unfi"
 fi
 
